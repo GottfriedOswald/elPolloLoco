cf4f98b2b9e180c6e5f3dd09b80e08e197819f21
GottfriedOswald <go.oswald@t-online.de>
2021-10-25 09:39:01 +0200

    set walking-images in a seperate array

--- models/character.class.js
+++ models/character.class.js
@@ -1,25 +1,25 @@
-class Character extends MovableObject{
-
-
+class Character extends MovableObject {
 
     height = 240;
     width = 122;
 
-    constructor(){
+    IMAGES_WALKING = [
+        'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-21.png',
+        'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-22.png',
+        'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-23.png',
+        'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-24.png',
+        'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-25.png',
+        'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-26.png'
+    ];
+
+    constructor() {
         super().loadImage('img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-21.png');
-        this.loadImages([
-            'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-21.png',
-            'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-22.png',
-            'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-23.png',
-            'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-24.png',
-            'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-25.png',
-            'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-26.png'
-        ]);
+        this.loadImages(this.IMAGES_WALKING);
         this.x = 80;
         this.y = 230;
     }
 
-    jump(){
+    jump() {
 
     }
 }
