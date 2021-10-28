b145c6c7684bdb1c9a87b2868f9e06597501dd63
GottfriedOswald <go.oswald@t-online.de>
2021-10-28 09:42:06 +0200

    add keyboard function

--- js/game.js
+++ js/game.js
@@ -14,9 +14,38 @@ function init() {
 }
 
 window.addEventListener('keydown',e => {
-    console.log(e);
+    // console.log(e);
     if (e.key === "ArrowUp") {
-        console.log('es wurde gedrückt');
-
+        keyboard.UP = true;
+    }
+    if (e.key === "ArrowDown") {
+        keyboard.DOWN = true;
+    }
+    if (e.key === "ArrowRight") {
+        keyboard.RIGHT = true;
+    }
+    if (e.key === "ArrowLeft") {
+        keyboard.LEFT = true;
+    }
+    if (e.key === " ") {
+        keyboard.SPACE = true;
+    }
+})
+window.addEventListener('keyup',e => {
+    // console.log(e);
+    if (e.key === "ArrowUp") {
+        keyboard.UP = false;
+    }
+    if (e.key === "ArrowDown") {
+        keyboard.DOWN = false;
+    }
+    if (e.key === "ArrowRight") {
+        keyboard.RIGHT = false;
+    }
+    if (e.key === "ArrowLeft") {
+        keyboard.LEFT = false;
+    }
+    if (e.key === " ") {
+        keyboard.SPACE = false;
     }
 })
